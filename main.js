a1130b169a6bafb0d7d4616e5f2860c4bc058067
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-03-08 18:52:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+noseX=0;
+noseY=0;
+function preload(){
+    mustache= loadImage('bigode.png');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
